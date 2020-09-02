bff2f5fcf3a1ebbbb6ef9831755d6b26259b4355
Paloma <paloma98dasilva@gmail.com>
2020-09-01 23:42:36 -0600

    Character walk and Jump

---
+++ 2670Project/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterMover : MonoBehaviour
+{
+    
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3f;
+    //public float fastMoveSpeed;
+    public float jumpForce = 10f;
+    //public int jumpCountMax;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal")*moveSpeed;
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+        }
+
+        if (controller.isGrounded)
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+        
+        controller.Move(movement*Time.deltaTime);
+    }
+}

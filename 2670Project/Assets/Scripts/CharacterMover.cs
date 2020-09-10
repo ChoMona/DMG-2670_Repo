9626552879f44f7f261b5bad8cfe8de3807dbc0a
Paloma <paloma98dasilva@gmail.com>
2020-09-10 16:37:00 -0600

    Fixing the Jump!

--- 2670Project/Assets/Scripts/CharacterMover.cs
+++ 2670Project/Assets/Scripts/CharacterMover.cs
@@ -19,31 +19,32 @@ public class CharacterMover : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
     }
-    
-    private void Update()
-    {
-        var vInput = Input.GetAxis("Vertical")*moveSpeed;
-        movement.Set(vInput,yVar,0);
 
-        var hInput = Input.GetAxis("Horizontal")*Time.deltaTime*rotateSpeed;
-        transform.Rotate(0,hInput,0);
+   private void Update()
+   {
+       var vInput = Input.GetAxis("Vertical") * moveSpeed;
+       movement.Set(vInput, yVar, 0);
 
-        yVar += gravity * Time.deltaTime;
+       var hInput = Input.GetAxis("Horizontal") * Time.deltaTime * rotateSpeed;
+       transform.Rotate(0, hInput, 0);
 
-        if (controller.isGrounded && movement.y < 0)
-        {
-            yVar = -1f;
-            jumpCount = 0;
-        }
+       yVar += gravity * Time.deltaTime;
 
-        if (Input.GetButton("Jump") && jumpCount < jumpCountMax)
-        {
-            yVar = jumpForce;
-            jumpCount++;
-        }
+       if (controller.isGrounded && movement.y < 0)
+       {
+           yVar = -1f;
+           jumpCount = 0;
+       }
 
-        movement = transform.TransformDirection(movement);
-        controller.Move(movement * Time.deltaTime);
+       if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+       {
+           yVar = jumpForce;
+           jumpCount++;
+       }
 
-    }
+       movement = transform.TransformDirection(movement);
+       controller.Move(movement * Time.deltaTime);
+       
+   }
 }
+
